bce7342f3a6e9eef3aadea948e105b38a4540b46
Elaidzha1940 <eli080293@gmail.com>
2023-06-06 04:13:48 +0700

    Glassmorphism

---
+++ Glassmorphism/ContentView.swift
@@ -0,0 +1,27 @@
+//  /*
+//
+//  Project: Glassmorphism
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 06.06.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+          
+            Text("Glassmorphism")
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Glassmorphism/GlassmorphismApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassmorphismApp.swift
+//  Glassmorphism
+//
+//  Created by Elaidzha Shchukin on 06.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct GlassmorphismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
